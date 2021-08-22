66496ea6eb04c106513e8fc3ab60059af194f223
Temple Okosun <okosun.temple@gmail.com>
2021-08-22 10:04:57 -0400

    HAPP-5: completed roomModel

---
+++ backend/models/room.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const roomSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    maxcount: {
+      type: Number,
+      required: true,
+    },
+    rentperday: {
+      type: Number,
+      required: true,
+    },
+    imageurls: [],
+    currentbookings: [],
+    type: {
+      type: String,
+    },
+    desription: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
+
+const roomModel = mongoose.model('rooms', roomSchema)
+module.exports = roomModel
