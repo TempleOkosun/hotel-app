f87e54d0dd592e863f9b9740c6558607227d295b
Temple Okosun <okosun.temple@gmail.com>
2021-08-22 06:47:18 -0400

    happ-3: added support for testing with cypress

---
+++ frontend-web/src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import './_Navbar.css'
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg">
+        <a className="navbar-brand" href="#">
+          THomes
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="/register">
+                Register
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/login">
+                Login
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  )
+}
+
+export default Navbar
