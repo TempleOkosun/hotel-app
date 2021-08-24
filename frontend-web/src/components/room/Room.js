c1b7901d0d607cdf93b0b0130371dacc709d3a9a
Temple Okosun <okosun.temple@gmail.com>
2021-08-24 09:33:54 -0400

    HAPP-6: completed room modal

--- frontend-web/src/components/room/Room.js
+++ frontend-web/src/components/room/Room.js
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
+
+// Components
+import { Modal, Button, Carousel } from 'react-bootstrap'
 
 // Css
 import './_Room.css'
 
 function Room({ room }) {
+  const [show, setShow] = useState(false)
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
   return (
-    <div className="row box-shadow">
+    <div className="row box-shadow text-left">
       <div className="col-md-4">
         <img src={room.image_urls[0]} alt="" className="small-img" />
       </div>
@@ -17,9 +24,36 @@ function Room({ room }) {
         <p>Type: {room.type}</p>
 
         <div>
-          <button className="btn btn-primary"> View Details</button>
+          <button className="btn btn-primary" onClick={handleShow}>
+            {' '}
+            View Details
+          </button>
         </div>
       </div>
+
+      <Modal show={show} onHide={handleClose} size="lg">
+        <Modal.Header>
+          <Modal.Title>{room.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Carousel prevLabel="" nextLabel="">
+            {room.image_urls.map((url) => {
+              return (
+                // eslint-disable-next-line react/jsx-key
+                <Carousel.Item>
+                  <img className="d-block w-100 big-img" src={url} alt="" />
+                </Carousel.Item>
+              )
+            })}
+          </Carousel>
+          <p>{room.description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
   )
 }
@@ -33,5 +67,6 @@ Room.propTypes = {
     max_count: PropTypes.number.isRequired,
     phone_number: PropTypes.string.isRequired,
     type: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
   }).isRequired,
 }
