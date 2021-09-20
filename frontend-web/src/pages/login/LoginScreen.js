05c15fd9d97bcd4055fc25377496f68b28ab369c
Temple Okosun <okosun.temple@gmail.com>
2021-09-20 14:11:30 -0400

    completed login & registration frontend

--- frontend-web/src/pages/login/LoginScreen.js
+++ frontend-web/src/pages/login/LoginScreen.js
@@ -1,7 +1,64 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+
+import { Button, Form } from 'react-bootstrap'
+
+import './_LoginScreen.css'
 
 function LoginScreen() {
-  return <div> Login</div>
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [confirmPassword, setConfirmPassword] = useState('')
+
+  function login(e) {
+    e.preventDefault()
+    if (password === confirmPassword) {
+      const user = {
+        name,
+        email,
+        password,
+        confirmPassword,
+      }
+      console.log(user)
+    } else {
+      alert('Passwords do not match')
+    }
+  }
+
+  return (
+    <div className="row justify-content-center mt-5">
+      <div className="col-md-5">
+        <div className="box-shadow">
+          <div>
+            <h2>Login</h2>
+            <Form>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formGroupPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit" onClick={login}>
+                Login
+              </Button>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default LoginScreen
--- frontend-web/src/pages/register/RegisterScreen.js
+++ frontend-web/src/pages/register/RegisterScreen.js
@@ -1,12 +1,17 @@
 import React, { useState, useEffect } from 'react'
 
+import { Button, Form } from 'react-bootstrap'
+
+import './_RegisterScreen.css'
+
 function RegisterScreen() {
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
 
-  function register() {
+  function register(e) {
+    e.preventDefault()
     if (password === confirmPassword) {
       const user = {
         name,
@@ -14,31 +19,58 @@ function RegisterScreen() {
         password,
         confirmPassword,
       }
+      console.log(user)
     } else {
       alert('Passwords do not match')
     }
   }
 
   return (
-    <div>
-      <div className="row justify-content-center mt-5">
-        <div className="col-md-5">
+    <div className="row justify-content-center mt-5">
+      <div className="col-md-5">
+        <div className="box-shadow">
           <div>
-            <h1>Register</h1>
-            <input
-              type="text"
-              className=" form control"
-              value={name}
-              onChange={(event) => setName(event.target.value)}
-              placeholder="Name"
-            />
-            <input type="text" className=" form control" placeholder="Email" />
-            <input type="text" className=" form control" placeholder="Password" />
-            <input type="text" className=" form control" placeholder="Confirm Password" />
-
-            <button className="btn btn-primary" onChange={register}>
-              Register
-            </button>
+            <h2>Register</h2>
+            <Form>
+              <Form.Group className="mb-3" controlId="formGroupName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control
+                  placeholder="Enter Name"
+                  value={name}
+                  onChange={(event) => setName(event.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formGroupEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="Enter email"
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formGroupPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Password"
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                />
+              </Form.Group>
+              <Form.Group className="mb-3" controlId="formGroupConfirmPassword">
+                <Form.Label>Confirm password</Form.Label>
+                <Form.Control
+                  type="password"
+                  placeholder="Confirm Password"
+                  value={confirmPassword}
+                  onChange={(event) => setConfirmPassword(event.target.value)}
+                />
+              </Form.Group>
+              <Button variant="primary" type="submit" onClick={register}>
+                Register
+              </Button>
+            </Form>
           </div>
         </div>
       </div>
